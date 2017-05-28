e3452cbb4b1f825e1cd47787ecfba3afce83cabf
Pavel Prorovsky <prorovsky@gmail.com>
2017-05-28 18:19:57 +0300

    clean up

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-    templateUrl: "landing.component.html"
+    templateUrl: "landing.component.html",
+    styles: ["* { text-align: center }"]
 })
 export class LandingComponent {
+    
+    constructor(private _router: Router){
 
+    }
+
+    goToList(): void {
+        this._router.navigate(["/hotels"]);
+    }
 }
